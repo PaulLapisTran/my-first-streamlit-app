0793b9d016020820709d94a2b9adf2d10c6ec4f0
Paul Tran <110105483+PaulLapisTran@users.noreply.github.com>
2023-03-11 10:42:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import plotly
 import numpy as np
 
 st.title('Giải phương trình bậc nhất')
-a = st.number_input('Tham số a', np.ninf, np.inf)
-b = st.number_input('Tham số b', np.ninf, np.inf)
+a = st.number_input('Tham số a', -float('inf'), float('inf'))
+b = st.number_input('Tham số b', -float('inf'), float('inf'))
 if st.button('Giải'):
     st.write('Phương trình có 1 nghiệm', -b/a)
