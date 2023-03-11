ee172d6d8435fb887b5c71712d77636ae290a9c4
Paul Tran <110105483+PaulLapisTran@users.noreply.github.com>
2023-03-11 10:33:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ import streamlit as st
 a = st.number_input('Tham số a', -1000, 1000)
 b = st.number_input('Tham số b', -1000, 1000)
 if st.button('Giải'):
-    st.text('Phương trình có 1 nghiệm', -b/a)
+    st.write('Phương trình có 1 nghiệm', -b/a)
