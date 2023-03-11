aae3235b5c1fb587332f354d5d61bdc1ac370e31
Paul Tran <110105483+PaulLapisTran@users.noreply.github.com>
2023-03-11 10:39:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import sklearn
 import plotly
+import numpy as np
 
 st.title('Giải phương trình bậc nhất')
-a = st.number_input('Tham số a', -1000, 1000)
-b = st.number_input('Tham số b', -1000, 1000)
+a = st.number_input('Tham số a', np.ninf, np.inf)
+b = st.number_input('Tham số b', np.ninf, np.inf)
 if st.button('Giải'):
     st.write('Phương trình có 1 nghiệm', -b/a)
